1b2073722d4f2a0670cbbb19731426c1cfda762b
Jisoo-Kim <ooSoo2Soo2oo@gmail.com>
2024-04-21 13:03:12 +0900

    4/19

--- javaLecture/java_practice/Recursive/src/MainClass.java
+++ javaLecture/java_practice/Recursive/src/MainClass.java
@@ -4,8 +4,11 @@ public class MainClass {
 	static long[] memo;
 	
 	public static void main(String[] args) {
-		memo = new long[101];
-		System.out.println(fibonacci(100));
+//		memo = new long[101];
+//		System.out.println(fibonacci(100));
+		
+		System.out.println(mySum(1));
+		
 		
 	}
 
@@ -18,6 +21,22 @@ public class MainClass {
 		}
 		else {
 			return memo[n] = fibonacci(n-1) + fibonacci(n-2);
+		}
+	}
+	
+	public static int recursiveSum(int n) { // 1부터 n까지의 합 - 내 코드
+		if (n == 0) {
+			return 0;
+		} else {
+			return n + recursiveSum(n - 1);
+		}
+	}
+	
+	public static int mySum(int n) { // n부터 10까지의 합 - 교수님 코드
+		if (n == 10) {
+			return 10;
+		} else {
+			return n + mySum(++n);			
 		}
 	}
 
